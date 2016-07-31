0e075b00c3e9b642413a3d099c5c8479d81bdbf2
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-31 20:25:51 -0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
 const {join} = require('path');
 
-module.exports = {
-  entry: {
-    'polyfills': './polyfills.ts',
-    'vendor': './vendor.ts',
-    'app': './index.ts',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    sourceMapFileName: '[name].map',
-    chunkFileName: '[id].chunk.js'
-  },
-  resolve: {
-    extensions: ['', '.ts', '.js', '.css', '.html'],
-    modulesDirectories: ['node_modules']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style!css!'
-      },
-      {
-        test: /\.ts$/,
-        exclude: /(node_modules)/,
-        loader: 'awesome-typescript-loader'
-      }
-    ]
+module.exports = (env) => {
+  return {
+    entry: {
+      'polyfills': './polyfills.ts',
+      'vendor': './vendor.ts',
+      'app': './index.ts',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      sourceMapFileName: '[name].map',
+      chunkFileName: '[id].chunk.js'
+    },
+    resolve: {
+      extensions: ['', '.ts', '.js', '.css', '.html'],
+      modulesDirectories: ['node_modules']
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: 'style!css!'
+        },
+        {
+          test: /\.ts$/,
+          exclude: /(node_modules)/,
+          loader: 'awesome-typescript-loader'
+        }
+      ]
+    }
   }
 }
